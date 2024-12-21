86b6e53b4dd5592acf8defeb90d1c4dd10730e4c
Rakesh-nayak547 <nayakrakesh2021@gmail.com>
2024-12-21 15:01:28 +0530

    Update ChatClient.java

--- ChatClient.java
+++ ChatClient.java
@@ -1,42 +1,4 @@
-/*import java.io.*;
-import java.net.*;
 
-public class ChatClient {
-    public static void main(String[] args) {
-        try (Socket socket = new Socket("localhost", 12345)) {
-            System.out.println("Connected to the server!");
-
-            // Input and output streams
-            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-
-            BufferedReader consoleInput = new BufferedReader(new InputStreamReader(System.in));
-
-            // Thread for reading messages from the server
-            Thread readThread = new Thread(() -> {
-                try {
-                    String serverMessage;
-                    while ((serverMessage = input.readLine()) != null) {
-                        System.out.println("Server: " + serverMessage);
-                    }
-                } catch (IOException e) {
-                    System.out.println("Server disconnected.");
-                }
-            });
-
-            readThread.start();
-
-            // Sending messages to the server
-            String clientMessage;
-            while ((clientMessage = consoleInput.readLine()) != null) {
-                output.println(clientMessage);
-            }
-
-        } catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-    }
-}*/
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
