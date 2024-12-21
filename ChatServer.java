5db8f4f2201b08c5885f25e2f89f7a6e7a13a802
Rakesh-nayak547 <nayakrakesh2021@gmail.com>
2024-12-21 15:01:56 +0530

    Update ChatServer.java

--- ChatServer.java
+++ ChatServer.java
@@ -1,45 +1,4 @@
-/*import java.io.*;
-import java.net.*;
 
-public class ChatServer {
-    public static void main(String[] args) {
-        try (ServerSocket serverSocket = new ServerSocket(12345)) {
-            System.out.println("Server is running and waiting for a client...");
-            
-            Socket clientSocket = serverSocket.accept();
-            System.out.println("Client connected!");
-
-            // Input and output streams
-            BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            PrintWriter output = new PrintWriter(clientSocket.getOutputStream(), true);
-
-            BufferedReader consoleInput = new BufferedReader(new InputStreamReader(System.in));
-
-            // Thread for reading messages from the client
-            Thread readThread = new Thread(() -> {
-                try {
-                    String clientMessage;
-                    while ((clientMessage = input.readLine()) != null) {
-                        System.out.println("Client: " + clientMessage);
-                    }
-                } catch (IOException e) {
-                    System.out.println("Client disconnected.");
-                }
-            });
-
-            readThread.start();
-
-            // Sending messages to the client
-            String serverMessage;
-            while ((serverMessage = consoleInput.readLine()) != null) {
-                output.println(serverMessage);
-            }
-
-        } catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-    }
-}*/
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
